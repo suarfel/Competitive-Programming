478fbd624efda00be8bf7b81555681e253d40bd7
surafel getahun <surafelgetahun01@gmail.com>
2022-05-20 22:06:51 +0300

    easy array algorithm

---
+++ moveZeros.py
@@ -0,0 +1,15 @@
+class Solution:
+    def moveZeroes(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        j = 0
+        i= 0
+        size = len(nums)
+        while i < size:
+            if nums[i] == 0:
+                nums.append(0)
+                nums.pop(i)
+                size = size -1
+            else:
+                i = i+1
