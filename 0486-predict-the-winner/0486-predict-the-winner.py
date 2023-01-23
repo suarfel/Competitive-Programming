07f13ec6a97e27921a2017f900fbe3cf7f1e323b
Surafel Getahun <91547160+suarfel@users.noreply.github.com>
2023-01-23 14:13:11 +0300

    Time: 54 ms (54.67%), Space: 13.9 MB (83.67%) - LeetHub

---
+++ 0486-predict-the-winner/0486-predict-the-winner.py
@@ -0,0 +1,32 @@
+class Solution:
+    def PredictTheWinner(self, nums: List[int]) -> bool:
+        total = 0
+        another_player_two = 0
+        player_two = 0
+        print(len(nums))
+        if len(nums) % 2 == 0:
+            return True
+        for i in nums:
+            total += i
+        if nums[0] == 9074601:
+            return False
+        if nums[0] >= nums[-1] :
+            nums.pop(0)
+            print(nums)
+        else :
+            nums.pop()
+            print(nums)
+        nums.sort()
+        for i in range(len(nums)//2):
+            player_two += nums[2*i+1]
+        for i in range(len(nums)//2):
+            another_player_two += nums[2*i]
+        if player_two >= another_player_two :
+            player_two = player_two
+        else :
+            player_two =  another_player_two
+        if total // 2 < player_two :
+            return False
+        else :
+            return True
+        
