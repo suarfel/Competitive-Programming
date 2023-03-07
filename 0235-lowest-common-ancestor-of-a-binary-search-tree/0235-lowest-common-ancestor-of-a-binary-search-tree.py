88e35891ea1b1486823c5e2879c9590816011462
Surafel Getahun <91547160+suarfel@users.noreply.github.com>
2023-03-07 22:23:47 +0300

    Time: 87 ms (38.05%), Space: 18.7 MB (96.48%) - LeetHub

--- 0235-lowest-common-ancestor-of-a-binary-search-tree/0235-lowest-common-ancestor-of-a-binary-search-tree.py
+++ 0235-lowest-common-ancestor-of-a-binary-search-tree/0235-lowest-common-ancestor-of-a-binary-search-tree.py
@@ -7,32 +7,9 @@
 
 class Solution:
     def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
-        if (root.val > p.val and root.val < q.val) or (root.val < p.val and root.val > q.val):
-            return root
-        elif root.val < p.val and root.val < q.val:
+        if root.val < p.val and root.val < q.val:
             return self.lowestCommonAncestor(root.right,p,q)
         elif root.val > p.val and root.val > q.val:
             return self.lowestCommonAncestor(root.left,p,q)
-        else:
-            return root
-        # elif root.val > p.val and root.
-#         arr_one = self.traversal(root,p)
-#         arr_two = self.traversal(root,q)
+        return root
         
-#         print(arr_one)
-#         print(arr_two)
-#         return arr_one[0]
-#     def traversal(self,root,p):
-#         arr = []
-#         if root.val > p.val:
-#             arr.append(root)
-#             arr += self.traversal(root.left,p)
-#             print(True)
-#         elif root.val < p.val:
-#             arr.append(root)
-#             arr += self.traversal(root.right,p)
-#         else:
-#             arr.append(root)
-#         return arr
-#     # def isNear(self, p):
-#     # self.distToPoint(p)
