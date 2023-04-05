52819966aee673b8ce29e9acf3ec54ce36ff858c
Surafel Getahun <91547160+suarfel@users.noreply.github.com>
2023-04-05 13:22:48 +0300

    Time: 5418 ms (34.40%), Space: 24.7 MB (62.00%) - LeetHub

--- 2523-closest-prime-numbers-in-range/2523-closest-prime-numbers-in-range.py
+++ 2523-closest-prime-numbers-in-range/2523-closest-prime-numbers-in-range.py
@@ -1,15 +1,12 @@
 import datetime 
 class Solution:
     def closestPrimes(self, m: int, n: int) -> List[int]:
-        st1 = datetime.datetime.now()
+
         is_prime = [True for i in range(n + 1)]
         ans = []
-        et1 = datetime.datetime.now()
-        print(et1-st1)
         is_prime[0] = is_prime[1] = False
         i = 2
         
-        st2 = datetime.datetime.now()
         while i * i <= n:
             if is_prime[i]:
                 j = i * i
@@ -17,11 +14,8 @@ class Solution:
                     is_prime[j] = False
                     j += i
             i += 1
-        et2 = datetime.datetime.now()
-        print(et2-st2)
-       
-            
-        st3 = datetime.datetime.now()
+         
+           
         arr = []
         for i in range(m, len(is_prime)):
             if is_prime[i]:
@@ -36,9 +30,7 @@ class Solution:
             if  difference > abs(arr[i]-arr[i+1]):
                 difference = abs(arr[i]-arr[i+1])
                 ans = [arr[i],arr[i+1]]
-        et3 = datetime.datetime.now()
-        print(et3-st3)
-                
+    
         return ans
  
         
