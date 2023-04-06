4bbe15158cd0f5726521f6d2493a16c47ae873b6
Surafel Getahun <91547160+suarfel@users.noreply.github.com>
2023-04-06 18:33:56 +0300

    Time: 726 ms (44.57%), Space: 16.7 MB (20.93%) - LeetHub

--- 2044-count-number-of-maximum-bitwise-or-subsets/2044-count-number-of-maximum-bitwise-or-subsets.py
+++ 2044-count-number-of-maximum-bitwise-or-subsets/2044-count-number-of-maximum-bitwise-or-subsets.py
@@ -1,27 +1,21 @@
 class Solution:
     def countMaxOrSubsets(self, nums: List[int]) -> int:
-        ans = self.backTrack(nums,[],[],0)
-        d = {}
+        ans = self.backTrack(nums,[],0,0)
         maxV = 1
+        d = {}
         for i in ans:
-            temp = 0
-            for j in range(len(i)):
-                temp |= i[j]
-            if maxV < temp :
-                maxV = temp
-            if temp not in d:
-                d[temp] = 1
+            if maxV < i:
+                maxV =i
+            if i not in d:
+                d[i] = 1
             else:
-                d[temp] += 1
-            
+                d[i] += 1
         return d[maxV]
                 
     def backTrack(self,nums,ans,temp,index):
-        if len(temp) != 0:
-            ans.append(list(temp))
+        if temp != 0:
+            ans.append(temp)
         for i in range(index,len(nums)):
-            temp.append(nums[i])
-            self.backTrack(nums,ans,temp,i+1)
-            temp.pop()
+            self.backTrack(nums,ans,temp | nums[i],i+1)
         return ans
     
