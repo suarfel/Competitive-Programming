f3aef4dee44745c0ffab0666a2c2bd96e2f1968b
Surafel Getahun <91547160+suarfel@users.noreply.github.com>
2023-04-30 19:40:52 +0300

    Time: 72 ms (12.10%), Space: 16.6 MB (12.96%) - LeetHub

--- 2460-apply-operations-to-an-array/2460-apply-operations-to-an-array.py
+++ 2460-apply-operations-to-an-array/2460-apply-operations-to-an-array.py
@@ -8,11 +8,11 @@ class Solution:
             if nums[i] == nums[i+1] :
                 nums[i] *= 2
                 nums[i+1] = 0
-        for i in range(len(nums)):
             if nums[i] != 0:
                 ans.append(nums[i])
             else:
                 arr.append(nums[i])
+        ans.append(nums[-1])
         return ans + arr
                 
         
