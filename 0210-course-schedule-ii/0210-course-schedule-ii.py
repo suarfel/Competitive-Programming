a52ce28468453f60819add8c4ea38e8852155d6d
Surafel Getahun <91547160+suarfel@users.noreply.github.com>
2023-05-08 19:52:02 +0300

    Time: 109 ms (33.59%), Space: 19.6 MB (6.80%) - LeetHub

--- 0210-course-schedule-ii/0210-course-schedule-ii.py
+++ 0210-course-schedule-ii/0210-course-schedule-ii.py
@@ -2,37 +2,57 @@ class Solution:
     def findOrder(self, numCourses: int, prerequisites: List[List[int]]) -> List[int]:
         
         incoming_degree = [0] * numCourses
-        
         graph = defaultdict(list)
         
         for course_one , course_two in prerequisites:
             graph[course_two].append(course_one)
             incoming_degree[course_one] += 1
-            
-            
-        queue = deque([])
+              
+        temp = []
        
         for i in range(len(incoming_degree)):
-        
             if 0 == incoming_degree[i] :
-                queue.append(i)
-         
-        ans = []
+                temp.append(i)
         
-        while queue :
-            
-            node = queue.popleft()
-            ans.append(node)
-            
-            for neghibor in graph[node]:
+        cycle = False
+        ans = []
+        def dfs(current):
+            nonlocal cycle
+            for neghibor in graph[current]:
                 
-                incoming_degree[neghibor] -= 1
-                if incoming_degree[neghibor] == 0:
-                    queue.append(neghibor)
-        if len(ans) != numCourses:
-            return []
+                if visited[neghibor] == 'white':
+                    visited[neghibor] = 'grey'
+                    dfs(neghibor)
+                elif visited[neghibor] == "grey":
+                    cycle = True
+           
+            if visited[current] != 'black':
+                ans.append(current)
+            visited[current] = 'black'
+          
             
+        visited = ['white'] * numCourses
+        
+        
+        for course in temp:
+            dfs(course)
+            
+         
+        if len(ans) != numCourses or cycle:
+            return []
+        ans.reverse()
         return ans
+        
+        
+        
+            
+        
+        
+        
+            
+         
+      
+      
             
             
             
