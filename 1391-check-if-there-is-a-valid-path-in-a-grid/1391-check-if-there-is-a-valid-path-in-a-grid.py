32d0712f595bd8ecdf5cbb0404ec068cdf2fee2e
Surafel Getahun <91547160+suarfel@users.noreply.github.com>
2023-05-17 23:23:21 +0300

    Time: 4842 ms (5.45%), Space: 53.2 MB (60.60%) - LeetHub

--- 1391-check-if-there-is-a-valid-path-in-a-grid/1391-check-if-there-is-a-valid-path-in-a-grid.py
+++ 1391-check-if-there-is-a-valid-path-in-a-grid/1391-check-if-there-is-a-valid-path-in-a-grid.py
@@ -1,48 +1,121 @@
 class Solution:
     def hasValidPath(self, grid: List[List[int]]) -> bool:
         
-        graph =  {}
-        
         graph = {1 : {'l','r'} , 2 : {'u','d'},3:{'u','l'},4:{'u','r'},5:{'d','l'},6:{'d','r'}}
-        
         directions = [(1,0),(0,1),(0,-1),(-1,0)]
-        visited = [[False for j in range(len(grid[0]))] for i in range(len(grid))]
-      
+        rep = defaultdict(tuple)
+        rank = defaultdict(int)
+
+    
+    
+        for i in range(len(grid)):
+            for j in range(len(grid[0])):
+                rep[(i,j)] = (i,j) 
+                rank[(i,j)] = 0
+                
         def in_bound(row,col):
-            return (0 <= row < len(grid)) and (0 <= col < len(grid[0]))
+            return (0 <= row < len(grid)) and ( 0 <= col < len(grid[0]))
         
-        def dfs_grid(row,col):
+        def find(x):
             
-            if row == len(grid) - 1 and col == len(grid[0]) - 1:
-                return True
-            visited[row][col] = True
+            root = x
+            while root != rep[root]:
+                root = rep[root] 
+            while x != root:
+                parent = rep[x]
+                rep[x] = root
+                x = parent
+            return root 
+        
+        def union(x,y):
             
-            for direction in directions:
-                new_row = row + direction[0]
-                new_col = col + direction[1]
-                if in_bound(new_row,new_col) and not visited[new_row][new_col]:
-                    if new_row == row and new_col > col:
-                        if 'r' in graph[grid[row][col]] and 'l' in graph[grid[new_row][new_col]]:
-                            r = dfs_grid(new_row,new_col)
-                            if r:
-                                return True
-                    elif new_row == row and new_col < col:
-                        if 'l' in graph[grid[row][col]] and 'r' in graph[grid[new_row][new_col]]:
-                            r = dfs_grid(new_row,new_col)
-                            if r:
-                                return True
-                    elif new_col == col and new_row > row:
-                        if 'u' in graph[grid[row][col]] and 'd' in graph[grid[new_row][new_col]]:
-                            r = dfs_grid(new_row,new_col)
-                            if r:
-                                return True
-                    elif new_col == col and new_row < row:
-                        if 'd' in graph[grid[row][col]] and 'u' in graph[grid[new_row][new_col]]: 
-                            r = dfs_grid(new_row,new_col)
-                            if r:
-                                return True
-            return False
-        return dfs_grid(0,0)
+            rootX = find(x)
+            rootY = find(y)
+            
+            if rootX != rootY:
+                if rank[rootX] > rank[rootY]:
+                    rep[rootY] = rootX
+                if rank[rootY] > rank[rootX]:
+                    rep[rootX] = rootY
+                if rank[rootX] == rank[rootY]:
+                    rep[rootY] = rootX
+                    rank[rootX] += 1
+                
+        for i in range(len(grid)):
+            for j in range(len(grid[0])):
+                
+                # if rep[(i,j)] == (0,0):
+                row = i
+                col = j
+                for dirc in directions:
+                    new_row = row + dirc[0]
+                    new_col = col + dirc[1]
+                    if in_bound(new_row,new_col):
+                        if new_row == row and new_col > col:
+                            if 'r' in graph[grid[row][col]] and 'l' in graph[grid[new_row][new_col]]:
+                                union((row,col),(new_row,new_col))
+                        elif new_row == row and new_col < col:
+                            if 'l' in graph[grid[row][col]] and 'r' in graph[grid[new_row][new_col]]:
+                                union((row,col),(new_row,new_col))
+
+                        elif new_col == col and new_row > row:
+                            if 'u' in graph[grid[row][col]] and 'd' in graph[grid[new_row][new_col]]:
+                                union((row,col),(new_row,new_col))
+                        elif new_col == col and new_row < row:
+                            if 'd' in graph[grid[row][col]] and 'u' in graph[grid[new_row][new_col]]: 
+                                union((row,col),(new_row,new_col))
+        # print(find((0,0))) 
+        # print(find((len(grid)-1,len(grid[0])-1)))
+        
+        return find((0,0)) == find((len(grid)-1,len(grid[0])-1))
+
+
+
+                
+        
+        
+#         graph =  {}
+        
+#         graph = {1 : {'l','r'} , 2 : {'u','d'},3:{'u','l'},4:{'u','r'},5:{'d','l'},6:{'d','r'}}
+        
+#         directions = [(1,0),(0,1),(0,-1),(-1,0)]
+#         visited = [[False for j in range(len(grid[0]))] for i in range(len(grid))]
+      
+#         def in_bound(row,col):
+#             return (0 <= row < len(grid)) and (0 <= col < len(grid[0]))
+        
+#         def dfs_grid(row,col):
+            
+#             if row == len(grid) - 1 and col == len(grid[0]) - 1:
+#                 return True
+#             visited[row][col] = True
+            
+#             for direction in directions:
+#                 new_row = row + direction[0]
+#                 new_col = col + direction[1]
+#                 if in_bound(new_row,new_col) and not visited[new_row][new_col]:
+#                     if new_row == row and new_col > col:
+#                         if 'r' in graph[grid[row][col]] and 'l' in graph[grid[new_row][new_col]]:
+#                             r = dfs_grid(new_row,new_col)
+#                             if r:
+#                                 return True
+#                     elif new_row == row and new_col < col:
+#                         if 'l' in graph[grid[row][col]] and 'r' in graph[grid[new_row][new_col]]:
+#                             r = dfs_grid(new_row,new_col)
+#                             if r:
+#                                 return True
+#                     elif new_col == col and new_row > row:
+#                         if 'u' in graph[grid[row][col]] and 'd' in graph[grid[new_row][new_col]]:
+#                             r = dfs_grid(new_row,new_col)
+#                             if r:
+#                                 return True
+#                     elif new_col == col and new_row < row:
+#                         if 'd' in graph[grid[row][col]] and 'u' in graph[grid[new_row][new_col]]: 
+#                             r = dfs_grid(new_row,new_col)
+#                             if r:
+#                                 return True
+#             return False
+#         return dfs_grid(0,0)
             
             
             
