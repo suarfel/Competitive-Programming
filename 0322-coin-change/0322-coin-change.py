dccfd374b10bfe1225afaa21ae3bea8e1994e99d
Surafel Getahun <91547160+suarfel@users.noreply.github.com>
2023-05-29 22:56:21 +0300

    Time: 1915 ms (13.74%), Space: 47.9 MB (5.01%) - LeetHub

--- 0322-coin-change/0322-coin-change.py
+++ 0322-coin-change/0322-coin-change.py
@@ -1,18 +1,18 @@
 class Solution:
     def coinChange(self, coins: List[int], amount: int) -> int:
         
-        @lru_cache(maxsize = None)
+        memo = {}
         def dp(target_sum):
             if target_sum > amount :
                 return float('inf')
             if target_sum == amount:
                 return 0
-            
-            _min = float('inf')
-            for coin in coins:
-                _min = min(_min,dp(target_sum + coin))
+            if  target_sum not in memo:
+                memo[target_sum] = float('inf')
+                for coin in coins:
+                    memo[target_sum] = min(memo[target_sum], dp(target_sum + coin))
                 
-            return 1 + _min
+            return memo[target_sum] + 1
         
         count = dp(0)
         if count == float('inf'):
