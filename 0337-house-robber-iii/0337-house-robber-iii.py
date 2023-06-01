8869e0067576fe75798600e64fab5baaa4dfdee4
Surafel Getahun <91547160+suarfel@users.noreply.github.com>
2023-06-01 23:21:10 +0300

    Time: 59 ms (56.64%), Space: 18.6 MB (29.24%) - LeetHub

--- 0337-house-robber-iii/0337-house-robber-iii.py
+++ 0337-house-robber-iii/0337-house-robber-iii.py
@@ -6,66 +6,12 @@
 #         self.right = right
 class Solution:
     def rob(self, root: Optional[TreeNode]) -> int:
-        
-        @lru_cache(None)
-        def dfs(node):
-            
-            if not node:
-                return 0
-            
-            # pick
-            pick = node.val
-            if node.left:
-                if node.left.left:
-                    pick += dfs(node.left.left) 
-                if node.left.right:
-                    pick += dfs(node.left.right)
-                    
-            if node.right:
-                if node.right.left:
-                    pick += dfs(node.right.left) 
-                if node.right.right:
-                    pick += dfs(node.right.right)
-            
-            not_pick = 0
-            # not pick
-            if node.right:
-                not_pick += dfs(node.right)
-                
-            if node.left:
-                    
-                not_pick += dfs(node.left)
-                    
-            return max(pick, not_pick)
-       
-    
-        return dfs(root)
-                
-                
-                
-            
-            
-            
-        
-        
-        
-        
-#          nonlocal ans
-#             if root.left and root.right:
-#                 left = dp_rob(root.left)
-#                 right = dp_rob(root.right)
-#                 ans = max(ans,left[1]+right[1] + root.val,left[0]+right[0])
-                
-#                 return (left[1]+right[1] + root.val,left[0]+right[0])
-#             elif root.left :
-#                 left = dp_rob(root.left)
-#                 ans = max(ans,left[0] , left[1] +  root.val)
-#                 return (left[1] + root.val,left[0])
-#             elif root.right:
-#                 right = dp_rob(root.right)
-#                 ans = max(ans,right[0] , right[1] +  root.val)
-#                 return (right[1] + root.val,right[0])
-#             else:
-#                 ans = max(ans,root.val)
-#                 return (root.val,0)
-        
+      
+        def dp_rob(root):
+            if root:
+                left = dp_rob(root.left)  
+                right = dp_rob(root.right)
+                return  (root.val + left[1] + right[1],max(left)+ max(right))
+            if not root:
+                return (0,0)
+        return max(dp_rob(root))
