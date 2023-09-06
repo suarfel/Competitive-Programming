9426aa6e85365e50eea5a14f3328166a965c2d17
Surafel Getahun <91547160+suarfel@users.noreply.github.com>
2023-09-06 23:36:16 +0300

    Time: 3081 ms (11.32%), Space: 16.2 MB (96.89%) - LeetHub

--- 0486-predict-the-winner/0486-predict-the-winner.py
+++ 0486-predict-the-winner/0486-predict-the-winner.py
@@ -1,32 +1,17 @@
 class Solution:
-    def PredictTheWinner(self, nums: List[int]) -> bool:
-        total = 0
-        another_player_two = 0
-        player_two = 0
-        print(len(nums))
-        if len(nums) % 2 == 0:
-            return True
-        for i in nums:
-            total += i
-        if nums[0] == 9074601:
-            return False
-        if nums[0] >= nums[-1] :
-            nums.pop(0)
-            print(nums)
-        else :
-            nums.pop()
-            print(nums)
-        nums.sort()
-        for i in range(len(nums)//2):
-            player_two += nums[2*i+1]
-        for i in range(len(nums)//2):
-            another_player_two += nums[2*i]
-        if player_two >= another_player_two :
-            player_two = player_two
-        else :
-            player_two =  another_player_two
-        if total // 2 < player_two :
-            return False
-        else :
-            return True
+    def predictTheWinner(self, nums: List[int]) -> bool:
+        
+        return self.predict(0,len(nums)-1,nums) >= 0
+        
+    def predict(self,start,end,nums):
+        if start == end:
+            return nums[start]
+        
+        left = nums[start] - self.predict(start+1,end,nums)
+        right = nums[end] - self.predict(start,end-1,nums)
+        
+        return max(left,right)
+        
+        
+         
         
