9c96d1b895fbc54e7cc12996896b241fe9d2cb4f
Surafel Getahun <91547160+suarfel@users.noreply.github.com>
2023-10-09 15:00:21 +0300

    Time: 32 ms (90.46%), Space: 16 MB (96.72%) - LeetHub

--- 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.py
+++ 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.py
@@ -1,21 +1,45 @@
 class Solution:
     def strStr(self, haystack: str, needle: str) -> int:
         
-        i,j = 0,0
-        temp = i
-        if len(haystack) < len(needle):
+        d_ = 27
+        
+        def hasher(word):
+            total = 0
+            n = len(word)
+            for i in range(len(word)):
+                total += (ord(word[i]) - ord('a') + 1) * d_**(n-1)
+                n -= 1
+            return total
+        
+        def add_last_poll_first(word,first,last):
+            val = word - (ord(first) - ord('a') + 1)* d_**(len(needle)-1)
+            val = val*d_ + (ord(last) - ord('a') + 1)
+            return val
+        
+        if len(needle) > len(haystack):
             return -1
-        while i < len(haystack):
-            if needle[j] == haystack[temp]:
-                j += 1
-                temp += 1
-            else:
-                j = 0
-                i += 1
-                temp = i
-                
-            if j == len(needle) :
-                return i 
-            if temp >= len(haystack):
-                return -1
+        
+        i = 0
+        j = len(needle)
+        
+        needle_hash = hasher(needle)
+        start = haystack[i:j]
+        haystack_hash = hasher(start)
+        if needle_hash == haystack_hash:
+            return 0
+        
+         
+        while i < len(haystack) and j < len(haystack):
+            haystack_hash = add_last_poll_first(haystack_hash,haystack[i],haystack[j])
+            i += 1
+            j += 1
+            if needle_hash == haystack_hash :
+                return i
         return -1
+            
+            
+             
+                
+
+                
+                
