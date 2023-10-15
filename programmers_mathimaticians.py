784b65c713e47dea5542902635a35d7481979461
Surafel Getahun <91547160+suarfel@users.noreply.github.com>
2023-10-15 10:30:04 +0300

    Create programmers_mathimaticians.py

---
+++ programmers_mathimaticians.py
@@ -0,0 +1,20 @@
+n = int(input())
+
+for _ in range(n):
+    n,m = [int(num) for num in input().split()]
+    
+    print(min(min(n,m), (n+m)//4))
+            
+        
+        
+        
+        
+    
+
+        
+    
+    
+    
+    
+    
+    
