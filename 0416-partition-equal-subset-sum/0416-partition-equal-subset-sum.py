ca632692c2cf50992031f9c2db1d831b6264898a
Surafel Getahun <91547160+suarfel@users.noreply.github.com>
2023-10-19 14:14:21 +0300

    Time: 609 ms (71.91%), Space: 156.9 MB (10.78%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.py
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.py
@@ -1,14 +1,14 @@
 class Solution:
     def canPartition(self, nums: List[int]) -> bool:
-        memo = {}
+        
+        @lru_cache(None)
         def dp(n,target_sum):
             
             if n >= len(nums) or target_sum <= 0:
                 return target_sum == 0
             
-            if (n,target_sum) not in memo:
-                memo[(n,target_sum)] = dp(n+1,target_sum-nums[n]) or dp(n+1,target_sum)
-            return memo[(n,target_sum)] 
+            return dp(n+1,target_sum-nums[n]) or dp(n+1,target_sum)
+              
         return sum(nums)%2 == 0 and dp(0,sum(nums)//2)
             
         
