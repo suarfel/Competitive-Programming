8772699db5d7d906b1cc5d42a376965e95ffaaab
Surafel Getahun <91547160+suarfel@users.noreply.github.com>
2023-10-22 17:40:35 +0300

    Time: 908 ms (89.29%), Space: 27.2 MB (61.73%) - LeetHub

--- 1793-maximum-score-of-a-good-subarray/1793-maximum-score-of-a-good-subarray.py
+++ 1793-maximum-score-of-a-good-subarray/1793-maximum-score-of-a-good-subarray.py
@@ -1,17 +1,29 @@
 class Solution:
     def maximumScore(self, nums: List[int], k: int) -> int:
+        
         nums.append(float('-inf'))
         max_score = 0
         stack = []
-        for i,val in enumerate(nums):
+        for idx,val in enumerate(nums):
             while stack and nums[stack[-1]] >= val:
                 temp = stack.pop()
                 left = stack[-1] if stack else -1
-                right = i
+                right = idx
                 if left < k < right:
                     score = nums[temp]*(right-left-1)
                     if score > max_score:
                         max_score = score
-            stack.append(i)
+            stack.append(idx)
         return max_score
+        
+        
+        
+        
+        
+        
+         
+        
+        
+        
+        
         
