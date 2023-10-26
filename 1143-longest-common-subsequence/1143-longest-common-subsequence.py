2e4b10346e35e90ffee67a6130a05c27c2c1f9ed
Surafel Getahun <91547160+suarfel@users.noreply.github.com>
2023-10-26 22:37:02 +0300

    Time: 2017 ms (5.01%), Space: 468.1 MB (5.10%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.py
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.py
@@ -1,33 +1,19 @@
 class Solution:
     def longestCommonSubsequence(self, text1: str, text2: str) -> int:
         
-        dp = [[0]*len(text2) for _ in range(len(text1))]
         
-        for i in range(len(text1)-1,-1,-1):
+        @lru_cache(None)
+        def dp(i,j):
             
-            for j in range(len(text2)-1,-1,-1):
-                
-                if i == len(text1)-1 and j == len(text2)-1:
-                    if text1[i] == text2[j]:
-                        dp[i][j] = 1
-    
-                elif i == len(text1)-1 :
-                    if text1[i] == text2[j]:
-                        dp[i][j] = max(1,dp[i][j+1])
-                    else:
-                        dp[i][j] = dp[i][j+1]
-                        
-                elif j == len(text2)-1:
-                    if text1[i] == text2[j]:
-                        dp[i][j] = max(1,dp[i+1][j])
-                    else:
-                        dp[i][j] = dp[i+1][j]
-                else:
-                    if text1[i] == text2[j]:
-                        dp[i][j] = max(1 + dp[i+1][j+1],dp[i][j+1],dp[i+1][j])
-                    else:
-                        dp[i][j] = max(dp[i][j+1],dp[i+1][j])
-        return dp[0][0]
+            if i >= len(text1) or j >= len(text2) :
+                return 0
+            
+            if text1[i] == text2[j] :
+                return max(1 + dp(i+1,j+1),dp(i,j+1),dp(i+1,j))
+            else:
+                return max(dp(i,j+1),dp(i+1,j))
+            
+        return dp(0,0)
                         
                     
                 
