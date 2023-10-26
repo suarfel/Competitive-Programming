a3dc678e048dc52ae81781019033f908657355ce
Surafel Getahun <91547160+suarfel@users.noreply.github.com>
2023-10-26 15:14:31 +0300

    Time: 1789 ms (7.50%), Space: 72.6 MB (5.42%) - LeetHub

---
+++ 0823-binary-trees-with-factors/0823-binary-trees-with-factors.py
@@ -0,0 +1,18 @@
+class Solution:
+    def numFactoredBinaryTrees(self, arr: List[int]) -> int:
+        
+        arr.sort()
+        counter = defaultdict(int)
+        
+        for i in range(len(arr)):
+            
+            ans = 1
+            for j in range(len(arr)):
+                
+                if arr[i] < arr[j]:
+                    break
+                ans += counter[arr[j]]*counter[arr[i]/arr[j]]
+            counter[arr[i]] = ans
+            
+        return sum(counter.values())%(pow(10,9) + 7)
+                    
