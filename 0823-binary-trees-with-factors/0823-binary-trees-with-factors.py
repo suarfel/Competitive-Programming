6ca9aa4cbe1b60758684250b5de71089cd62e201
Surafel Getahun <91547160+suarfel@users.noreply.github.com>
2023-10-26 16:53:37 +0300

    Time: 352 ms (56.67%), Space: 18.3 MB (11.67%) - LeetHub

--- 0823-binary-trees-with-factors/0823-binary-trees-with-factors.py
+++ 0823-binary-trees-with-factors/0823-binary-trees-with-factors.py
@@ -1,19 +1,28 @@
 class Solution:
     def numFactoredBinaryTrees(self, arr: List[int]) -> int:
-        
+
+        nums_set = set(arr)
         arr.sort()
-        counter = defaultdict(int)
+        arr.reverse()
+        count_hold = {}
+        
+        for idx,val in enumerate(arr):
+            count_hold[val] = idx 
+        
+        @lru_cache(None)
+        def dp(idx):
+            if idx >= len(arr):
+                return 0
+            ans = 1
+            for i in range(idx,len(arr)):
+                if arr[idx] % arr[i] == 0 and arr[idx]//arr[i] in nums_set:
+                    ans += dp(i)*dp(count_hold[arr[idx]//arr[i]])
+            return ans
+        
+        total = 0
         
         for i in range(len(arr)):
-            
-            ans = 1
-            for j in range(len(arr)):
-                
-                if arr[i] < arr[j]:
-                    break
-                if arr[i] % arr[j] == 0:
-                    ans += counter[arr[j]]*counter[arr[i]//arr[j]]
-            counter[arr[i]] = ans
-             
-        return sum(counter.values())%(pow(10,9) + 7)
+            total += dp(i)
+        
+        return total % (pow(10,9) + 7)
                     
