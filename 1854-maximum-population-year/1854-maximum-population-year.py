c38f3ebe3a64f0590ae5718a332c74b61aa87e11
Surafel Getahun <91547160+suarfel@users.noreply.github.com>
2023-10-28 15:16:02 +0300

    Time: 55 ms (25.89%), Space: 16.4 MB (5.68%) - LeetHub

--- 1854-maximum-population-year/1854-maximum-population-year.py
+++ 1854-maximum-population-year/1854-maximum-population-year.py
@@ -16,11 +16,22 @@ class Solution:
         for i in range(1,len(arr)):
             arr[i] = (arr[i][0] + arr[i-1][0],arr[i][1])
         
-        ans = sorted(arr,reverse = True)
+        population, early_year = 0,0
         
-        for i in range(1,len(ans)):
-            if ans[i][0] < ans[i-1][0]:
-                return ans[i-1][1]
+        for count,year in arr:
+            
+            if count > population:
+                population = count
+                early_year = year
+            elif count == population and year < early_year:
+                early_year = year
+        
+        return early_year
+                
+                
+                
+        
+         
         
 
             
